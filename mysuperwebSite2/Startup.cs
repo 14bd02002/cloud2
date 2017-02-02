5bc420fcb46164a2f5dff34e2fdcbf407dbaa959
14bd02002 <dias8888d@gmail.com>
2017-02-02 21:12:35 +0600

    init

---
+++ mysuperwebSite2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(mysuperwebSite2.Startup))]
+namespace mysuperwebSite2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
